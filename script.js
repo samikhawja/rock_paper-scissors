957425167f821a09023271b43dddb6aa7e20a89e
samikhawja <skhawja11@gmail.com>
2021-09-22 11:13:31 -0700

    intial add with working RPS

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 var choiceArray = ["R", "P", "S"];
 
 function playRPS() {
+  var confirm = true
+    while (confirm) {
     var choice = prompt("Choose R, P or S:");
 
     var computerChoice = Math.floor(Math.random(0, 3));
 
-    var choiceIndex = choiceArray.indexOf(choice);
+    var choiceIndex = choiceArray.indexOf(choice.toUpperCase);
 
     var result = choiceIndex - computerChoice;
 
     var winCount = 0;
     var tieCount = 0;
     var lossCount = 0;
-    
+
     // -2. You win
 
     // -1. Computer wins
@@ -23,6 +25,18 @@ function playRPS() {
 
     // 2 Computer wins
 
-
-    
+    if (result === 0) {
+      tieCount += 1;
+      alert("You tied with the computer");
+    } else if (result === 1 || result === -2) {
+      winCount += 1;
+      alert("You beat the computer");
+    } else {
+      lossCount += 1;
+      alert("The computer beat you");
+    }
+    alert("tieCount" + tieCount);
+    confirm = confirm("Would you like to play again?");
+  }
 }
+playRPS();
