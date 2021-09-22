bd45143c7efb084caf0e0b208fdef80ebf58bae2
Andres Liu <andres.liu22@gmail.com>
2021-09-22 11:38:13 -0700

    finish Rock paper scissors

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
 var choiceArray = ["R", "P", "S"];
+  
+var winCount = 0;
+var tieCount = 0;
+var lossCount = 0;
+
 
 function playRPS() {
-    var choice = prompt("Choose R, P or S:");
+    var confirmChoice = true;
+    while (confirmChoice) {
+          
+        // -2. You win
+        // -1. Computer wins
+        // 0. Tie
+        // 1. You win
+        // 2 Computer wins
 
-    var computerChoice = Math.floor(Math.random(0, 3));
+        var choice = prompt("Choose R, P or S:");
 
-    var choiceIndex = choiceArray.indexOf(choice);
+        var computerChoice = Math.floor(Math.random() * 3);
 
-    var result = choiceIndex - computerChoice;
+        var choiceIndex = choiceArray.indexOf(choice.toUpperCase());
 
-    var winCount = 0;
-    var tieCount = 0;
-    var lossCount = 0;
-    
-    // -2. You win
+        // if (choiceIndex != 0 || choiceIndex != 1 || choiceIndex != 2) {
+        //     while (choiceIndex != 0 || choiceIndex != 1 || choiceIndex != 2) {
+        //         choice = prompt("Choose R, P or S:");
+        //         choiceIndex = choiceArray.indexOf(choice.toUpperCase());
+        //     }
+        // }
+        
+        var result = choiceIndex - computerChoice;
 
-    // -1. Computer wins
+        if (result === 0) {
+            tieCount += 1;
+            alert("You tied with the computer");
+        } else if (result === 1 || result === -2) {
+            winCount += 1;
+            alert("You beat the computer");
+        } else {
+            lossCount += 1;
+            alert("The computer beat you");
+        }
+        alert("Tie count: " + tieCount +
+                "\nWin Count: " + winCount +
+                "\nLose Count: " + lossCount);
 
-    // 0. Tie
+        var confirmChoice = confirm("Would you like to play again?");
 
-    // 1. You win
-
-    // 2 Computer wins
-
-
-    
-}
+    }
+  }
+  
+  playRPS();
