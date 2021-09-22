8e2c0be01513107bdd434adafb93d0b61e242cdd
Andres Liu <andres.liu22@gmail.com>
2021-09-22 10:49:20 -0700

    Setup logic

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+var choiceArray = ["R", "P", "S"];
 
 function playRPS() {
     var choice = prompt("Choose R, P or S:");
+
+    var computerChoice = Math.floor(Math.random(0, 3));
+
+    var choiceIndex = choiceArray.indexOf(choice);
+
+    var result = choiceIndex - computerChoice;
+
+    var winCount = 0;
+    var tieCount = 0;
+    var lossCount = 0;
+    
+    // -2. You win
+
+    // -1. Computer wins
+
+    // 0. Tie
+
+    // 1. You win
+
+    // 2 Computer wins
+
+
+    
 }
